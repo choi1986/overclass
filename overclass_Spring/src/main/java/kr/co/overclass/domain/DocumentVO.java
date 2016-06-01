968b19660eb8c63d4307a7a078ab47d8289ce2e8
misun <misun@192.168.0.153>
2016-06-01 16:59:20 +0900

    VO 수정

--- overclass_Spring/src/main/java/kr/co/overclass/domain/DocumentVO.java
+++ overclass_Spring/src/main/java/kr/co/overclass/domain/DocumentVO.java
@@ -3,164 +3,94 @@ package kr.co.overclass.domain;
 import java.util.Date;
 
 public class DocumentVO {
-	private String user_id;
-	private String user_pwd;
-	private String user_name;
-	private String user_email;
-	private String user_tel;
-	private String user_loc;
-	private String user_hobby;
-	private String user_birth;
-	private String user_gender;
-	private int user_pwdq;
-	private String user_pwda;
-	private Date user_joindate;
-	private String user_image;
-	private int user_logincnt;
-	private int user_admin;
+	private int dno;
+	private String writer;
+	private String content;
+	private String tag;
+	private Date writedate;
+	private String image;
+	private int goodcnt;
+	private int replycnt;
 
 	public DocumentVO() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public String toString() {
-		return "[아이디: "+user_id+
-				", 비번: "+user_pwd+
-				", 이름: "+user_name+
-				", 이메일: "+user_email+
-				", 전화번호: "+user_tel+
-				", 지역: "+user_loc+
-				", 취미: "+user_hobby+
-				", 생일: "+user_birth+
-				", 성별: "+user_gender+
-				", 질문: "+user_pwdq+
-				", 답변: "+user_pwda+
-				", 가입일: "+user_joindate+
-				", 이미지: "+user_image+
-				", 로그인실패횟수: "+user_logincnt+
-				", 관리자여부: "+user_admin+
+		return "[글번호: "+dno+
+				", 글쓴이: "+writer+
+				", 글내용: "+content+
+				", 태그: "+tag+
+				", 글작성일자: "+writedate+
+				", 이미지경로: "+image+
+				", 좋아요개수: "+goodcnt+
+				", 댓글개수: "+replycnt+
 				"]";
 	}
-	
-	public String getUser_id() {
-		return user_id;
+
+	public int getDno() {
+		return dno;
 	}
 
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+	public void setDno(int dno) {
+		this.dno = dno;
 	}
 
-	public String getUser_pwd() {
-		return user_pwd;
+	public String getWriter() {
+		return writer;
 	}
 
-	public void setUser_pwd(String user_pwd) {
-		this.user_pwd = user_pwd;
+	public void setWriter(String writer) {
+		this.writer = writer;
 	}
 
-	public String getUser_name() {
-		return user_name;
+	public String getContent() {
+		return content;
 	}
 
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
+	public void setContent(String content) {
+		this.content = content;
 	}
 
-	public String getUser_email() {
-		return user_email;
+	public String getTag() {
+		return tag;
 	}
 
-	public void setUser_email(String user_email) {
-		this.user_email = user_email;
+	public void setTag(String tag) {
+		this.tag = tag;
 	}
 
-	public String getUser_tel() {
-		return user_tel;
+	public Date getWritedate() {
+		return writedate;
 	}
 
-	public void setUser_tel(String user_tel) {
-		this.user_tel = user_tel;
+	public void setWritedate(Date writedate) {
+		this.writedate = writedate;
 	}
 
-	public String getUser_loc() {
-		return user_loc;
+	public String getImage() {
+		return image;
 	}
 
-	public void setUser_loc(String user_loc) {
-		this.user_loc = user_loc;
+	public void setImage(String image) {
+		this.image = image;
 	}
 
-	public String getUser_hobby() {
-		return user_hobby;
+	public int getGoodcnt() {
+		return goodcnt;
 	}
 
-	public void setUser_hobby(String user_hobby) {
-		this.user_hobby = user_hobby;
+	public void setGoodcnt(int goodcnt) {
+		this.goodcnt = goodcnt;
 	}
 
-	public String getUser_birth() {
-		return user_birth;
+	public int getReplycnt() {
+		return replycnt;
 	}
 
-	public void setUser_birth(String user_birth) {
-		this.user_birth = user_birth;
-	}
-
-	public String getUser_gender() {
-		return user_gender;
-	}
-
-	public void setUser_gender(String user_gender) {
-		this.user_gender = user_gender;
-	}
-
-	public int getUser_pwdq() {
-		return user_pwdq;
-	}
-
-	public void setUser_pwdq(int user_pwdq) {
-		this.user_pwdq = user_pwdq;
-	}
-
-	public String getUser_pwda() {
-		return user_pwda;
-	}
-
-	public void setUser_pwda(String user_pwda) {
-		this.user_pwda = user_pwda;
-	}
-
-	public Date getUser_joindate() {
-		return user_joindate;
-	}
-
-	public void setUser_joindate(Date user_joindate) {
-		this.user_joindate = user_joindate;
-	}
-
-	public String getUser_image() {
-		return user_image;
-	}
-
-	public void setUser_image(String user_image) {
-		this.user_image = user_image;
-	}
-
-	public int getUser_logincnt() {
-		return user_logincnt;
-	}
-
-	public void setUser_logincnt(int user_logincnt) {
-		this.user_logincnt = user_logincnt;
-	}
-
-	public int getUser_admin() {
-		return user_admin;
-	}
-
-	public void setUser_admin(int user_admin) {
-		this.user_admin = user_admin;
+	public void setReplycnt(int replycnt) {
+		this.replycnt = replycnt;
 	}
 	
 	
--- overclass_Spring/src/main/java/kr/co/overclass/domain/UserVO.java
+++ overclass_Spring/src/main/java/kr/co/overclass/domain/UserVO.java
@@ -3,94 +3,164 @@ package kr.co.overclass.domain;
 import java.util.Date;
 
 public class UserVO {
-	private int dno;
-	private String writer;
-	private String content;
-	private String tag;
-	private Date writedate;
-	private String image;
-	private int goodcnt;
-	private int replycnt;
+	private String user_id;
+	private String user_pwd;
+	private String user_name;
+	private String user_email;
+	private String user_tel;
+	private String user_loc;
+	private String user_hobby;
+	private String user_birth;
+	private String user_gender;
+	private int user_pwdq;
+	private String user_pwda;
+	private Date user_joindate;
+	private String user_image;
+	private int user_logincnt;
+	private int user_admin;
 
 	public UserVO() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public String toString() {
-		return "[글번호: "+dno+
-				", 작성자: "+writer+
-				", 내용: "+content+
-				", 태그: "+tag+
-				", 작성일: "+writedate+
-				", 이미지: "+image+
-				", 좋아요: "+goodcnt+
-				", 댓글: "+replycnt+
+		return "[사용자ID: "+user_id+
+				", 사용자암호: "+user_pwd+
+				", 이름: "+user_name+
+				", 이메일: "+user_email+
+				", 핸드폰번호: "+user_tel+
+				", 지역: "+user_loc+
+				", 취미: "+user_hobby+
+				", 생일: "+user_birth+
+				", 성별: "+user_gender+
+				", 비밀번호질문: "+user_pwdq+
+				", 비밀번호답: "+user_pwda+
+				", 가입일자: "+user_joindate+
+				", 프로필사진: "+user_image+
+				", 로그인횟수: "+user_logincnt+
+				", 관리자: "+user_admin+
 				"]";
 	}
-
-	public int getDno() {
-		return dno;
+	
+	public String getUser_id() {
+		return user_id;
 	}
 
-	public void setDno(int dno) {
-		this.dno = dno;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
 	}
 
-	public String getWriter() {
-		return writer;
+	public String getUser_pwd() {
+		return user_pwd;
 	}
 
-	public void setWriter(String writer) {
-		this.writer = writer;
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
 	}
 
-	public String getContent() {
-		return content;
+	public String getUser_name() {
+		return user_name;
 	}
 
-	public void setContent(String content) {
-		this.content = content;
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
 
-	public String getTag() {
-		return tag;
+	public String getUser_email() {
+		return user_email;
 	}
 
-	public void setTag(String tag) {
-		this.tag = tag;
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
 	}
 
-	public Date getWritedate() {
-		return writedate;
+	public String getUser_tel() {
+		return user_tel;
 	}
 
-	public void setWritedate(Date writedate) {
-		this.writedate = writedate;
+	public void setUser_tel(String user_tel) {
+		this.user_tel = user_tel;
 	}
 
-	public String getImage() {
-		return image;
+	public String getUser_loc() {
+		return user_loc;
 	}
 
-	public void setImage(String image) {
-		this.image = image;
+	public void setUser_loc(String user_loc) {
+		this.user_loc = user_loc;
 	}
 
-	public int getGoodcnt() {
-		return goodcnt;
+	public String getUser_hobby() {
+		return user_hobby;
 	}
 
-	public void setGoodcnt(int goodcnt) {
-		this.goodcnt = goodcnt;
+	public void setUser_hobby(String user_hobby) {
+		this.user_hobby = user_hobby;
 	}
 
-	public int getReplycnt() {
-		return replycnt;
+	public String getUser_birth() {
+		return user_birth;
 	}
 
-	public void setReplycnt(int replycnt) {
-		this.replycnt = replycnt;
+	public void setUser_birth(String user_birth) {
+		this.user_birth = user_birth;
+	}
+
+	public String getUser_gender() {
+		return user_gender;
+	}
+
+	public void setUser_gender(String user_gender) {
+		this.user_gender = user_gender;
+	}
+
+	public int getUser_pwdq() {
+		return user_pwdq;
+	}
+
+	public void setUser_pwdq(int user_pwdq) {
+		this.user_pwdq = user_pwdq;
+	}
+
+	public String getUser_pwda() {
+		return user_pwda;
+	}
+
+	public void setUser_pwda(String user_pwda) {
+		this.user_pwda = user_pwda;
+	}
+
+	public Date getUser_joindate() {
+		return user_joindate;
+	}
+
+	public void setUser_joindate(Date user_joindate) {
+		this.user_joindate = user_joindate;
+	}
+
+	public String getUser_image() {
+		return user_image;
+	}
+
+	public void setUser_image(String user_image) {
+		this.user_image = user_image;
+	}
+
+	public int getUser_logincnt() {
+		return user_logincnt;
+	}
+
+	public void setUser_logincnt(int user_logincnt) {
+		this.user_logincnt = user_logincnt;
+	}
+
+	public int getUser_admin() {
+		return user_admin;
+	}
+
+	public void setUser_admin(int user_admin) {
+		this.user_admin = user_admin;
 	}
 	
 	
