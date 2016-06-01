40a5aa22b358a8e4756f7b5b3eea10b468b705d4
WooSangHee <sanghee199@naver.com>
2016-06-01 11:14:53 +0900

    msg<->report 바뀜

--- overclass_Spring/src/main/webapp/resources/sql/msg.sql
+++ overclass_Spring/src/main/webapp/resources/sql/msg.sql
@@ -1,8 +1,7 @@
-drop table report;
-create table report(
-	rep_num number primary key,
-	user_id varchar2(12) not null,
-	doc_num number not null,
-	rep_content varchar2(120),
-	rep_date date	-- 신고처리일자
+drop table msg;
+create table msg(
+	msg_sender varchar2(12) not null,
+	msg_receiver varchar2(12) not null,
+	msg_content varchar2(210) not null,
+	msg_writedate date default sysdate
 );
--- overclass_Spring/src/main/webapp/resources/sql/report.sql
+++ overclass_Spring/src/main/webapp/resources/sql/report.sql
@@ -1,7 +1,8 @@
-drop table msg;
-create table msg(
-	msg_sender varchar2(12) not null,
-	msg_receiver varchar2(12) not null,
-	msg_content varchar2(210) not null,
-	msg_writedate date default sysdate
+drop table report;
+create table report(
+	rep_num number primary key,
+	user_id varchar2(12) not null,
+	doc_num number not null,
+	rep_content varchar2(120),
+	rep_date date	-- 신고처리일자
 );
