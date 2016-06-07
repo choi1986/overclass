4b03c79bc6b0cc1a4ac2bb1907526af9d358f5eb
WooSangHee <sanghee199@naver.com>
2016-06-07 11:51:57 +0900

    UserService

---
+++ overclass_Spring/src/main/java/kr/co/overclass/service/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package kr.co.overclass.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.overclass.domain.UserVO;
+import kr.co.overclass.persistence.UserDAO;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Inject
+	private UserDAO userDAO;
+
+	@Override
+	public UserVO searchUser(String user_id) throws Exception {
+		return userDAO.searchUser(user_id);
+	}
+
+	@Override
+	public void createUser(UserVO user) throws Exception {
+		userDAO.createUser(user);
+	}
+
+	@Override
+	public void updateUser(UserVO user) throws Exception {
+		userDAO.updateUser(user);
+	}
+
+	@Override
+	public void updateUserImg(String user_image) throws Exception {
+		userDAO.updateUserImg(user_image);
+	}
+
+	@Override
+	public void deleteUser(String user_id) throws Exception {
+		userDAO.deleteUser(user_id);
+	}
+
+	@Override
+	public void login(String user_id, String user_pwd) throws Exception {
+		userDAO.login(user_id, user_pwd);
+	}
+
+}
