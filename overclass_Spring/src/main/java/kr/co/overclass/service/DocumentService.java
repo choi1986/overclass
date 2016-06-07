08936bc994a369e8bd9910dcf45d31c6ba4d3de6
욱이 <@192.168.0.143>
2016-06-07 15:09:43 +0900

    document dao, service, xml 작성

--- overclass_Spring/src/main/java/kr/co/overclass/service/DocumentService.java
+++ overclass_Spring/src/main/java/kr/co/overclass/service/DocumentService.java
@@ -1,5 +1,12 @@
 package kr.co.overclass.service;
 
-public interface DocumentService {
+import java.util.List;
 
+import kr.co.overclass.domain.DocumentVO;
+
+public interface DocumentService {
+	public void create(DocumentVO vo) throws Exception; //글쓰기
+	public void delete(int dno)throws Exception; //특정글 삭제
+	public List<DocumentVO> mainFeed_list() throws Exception; //메인피드 게시글 조회
+	public List<DocumentVO> myFeed_list() throws Exception; //마이피드 게시글 조회
 }
