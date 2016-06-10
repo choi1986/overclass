79f92a910c929277d9a7c4b4f2c7934fe8ad8440
욱이 <@192.168.0.109>
2016-06-10 15:33:40 +0900

    도큐먼트sql 수정

--- overclass_Spring/src/main/webapp/resources/sql/document.sql
+++ overclass_Spring/src/main/webapp/resources/sql/document.sql
@@ -2,7 +2,7 @@ drop table oc_document cascade constraints;
 create table oc_document(
 	dno number primary key,
 	writer varchar2(12) not null,
-	content varchar2(210) not null,
+	content varchar2(600) not null,
 	tag varchar2(210),
 	writedate date default sysdate,
 	image varchar2(120),
