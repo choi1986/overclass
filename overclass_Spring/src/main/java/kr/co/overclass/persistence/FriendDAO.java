926aac3d65f44bdc68d88e35ef60d080f80db4cc
BAE <BAE@192.168.0.101>
2016-06-15 10:18:07 +0900

    FriendDAO,FriendDAOImpl 추가(커밋을 안하여 데이터 완전 날라가서 다시 등록)

---
+++ overclass_Spring/src/main/java/kr/co/overclass/persistence/FriendDAO.java
@@ -0,0 +1,12 @@
+package kr.co.overclass.persistence;
+
+import kr.co.overclass.domain.FriendVO;
+
+public interface FriendDAO {
+	public void friend_rel_insert(FriendVO vo) throws Exception;//模备 包拌 涝仿
+	public void friend_rel_delete(FriendVO vo) throws Exception;//模备 包拌 昏力
+	public void friend_rel_select(FriendVO vo) throws Exception;//模备 包拌 八祸(1疙)
+	public void friend_req_insert(FriendVO vo) throws Exception;//模备 夸没 涝仿
+	public void friend_req_delete(FriendVO vo) throws Exception;//模备 夸没 昏力
+	public void friend_req_select(FriendVO vo) throws Exception;//模备 包拌 八祸(1疙)
+}
---
+++ overclass_Spring/src/main/java/kr/co/overclass/persistence/FriendDAOImpl.java
@@ -0,0 +1,43 @@
+package kr.co.overclass.persistence;
+
+import kr.co.overclass.domain.FriendVO;
+
+public class FriendDAOImpl implements FriendDAO {
+
+	@Override
+	public void friend_rel_insert(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void friend_rel_delete(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void friend_rel_select(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void friend_req_insert(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void friend_req_delete(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void friend_req_select(FriendVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
