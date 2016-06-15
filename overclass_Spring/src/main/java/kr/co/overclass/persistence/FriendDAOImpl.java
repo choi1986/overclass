36bc8be5ef56fd6af86312948332ffa893949496
BAE <BAE@192.168.0.101>
2016-06-15 11:02:44 +0900

    friend.xml id명 변경 FriendDAOImpl 추가

--- overclass_Spring/src/main/java/kr/co/overclass/persistence/FriendDAOImpl.java
+++ overclass_Spring/src/main/java/kr/co/overclass/persistence/FriendDAOImpl.java
@@ -1,43 +1,49 @@
 package kr.co.overclass.persistence;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
 import kr.co.overclass.domain.FriendVO;
 
+@Repository
 public class FriendDAOImpl implements FriendDAO {
 
+	private static final Logger logger = LoggerFactory.getLogger(FriendDAOImpl.class);
+	
+	@Inject
+	private SqlSession session;
+	
 	@Override
-	public void friend_rel_insert(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_insert_rel(FriendVO vo) throws Exception {
+		session.insert("friend.insert_rel",vo);
 	}
 
 	@Override
-	public void friend_rel_delete(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_delete_rel(FriendVO vo) throws Exception {
+		session.delete("friend.delete_rel",vo);
 	}
 
 	@Override
-	public void friend_rel_select(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_select_rel(FriendVO vo) throws Exception {
+		session.selectList("friend.select_rel", vo);
 	}
 
 	@Override
-	public void friend_req_insert(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_insert_req(FriendVO vo) throws Exception {
+		session.insert("friend.insert_rel",vo);
 	}
 
 	@Override
-	public void friend_req_delete(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_delete_req(FriendVO vo) throws Exception {
+		session.delete("friend.delete_rel",vo);
 	}
 
 	@Override
-	public void friend_req_select(FriendVO vo) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void oc_select_req(FriendVO vo) throws Exception {
+		session.selectList("friend.select_rel",vo);
 	}
-
 }
